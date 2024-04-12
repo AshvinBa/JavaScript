394afeea7a5b50b28304f197e645692cc821e8f5
Ashvin Bari <ashvinbari6@gmail.com>
2024-04-13 00:08:59 +0530

    commited
    13-04-2024

--- Projects/myntra_project/myntra-clone/script/bag.js
+++ Projects/myntra_project/myntra-clone/script/bag.js
@@ -1,57 +1,59 @@
+const CONVENIENCE_FEES = 99;
 let bagItemObjects;
 onLoad();
 
-function onLoad()
-{
-    loadBagItemObjects();
-    displayBagItem();
-    displayBagSummary();
+function onLoad() {
+  loadBagItemObjects();
+  displayBagItems();
+  displayBagSummary();
 }
 
-function displayBagSummary()
-{
-  let bagsummaryElement= document.querySelector('.bag-summary');
-  let totalItem = 10;
-  let totalMRP = 10;
-  let totalDiscount = 10;
-  let finalPayment = 10;
-  let Convenience = 10;
-  bagsummaryElement.innerHTML=
-  `<div class="bag-details-container">
-  <div class="price-header">PRICE DETAILS (${totalItem} Items) </div>
-  <div class="price-item">
-    <span class="price-item-tag">Total MRP</span>
-    <span class="price-item-value">Rs${totalMRP}</span>
+function displayBagSummary() {
+  let bagSummaryElement = document.querySelector('.bag-summary');
+  let totalItem = bagItemObjects.length;
+  let totalMRP = 0;
+  let totalDiscount = 0;
+
+  bagItemObjects.forEach(bagItem => {
+    totalMRP += bagItem.original_price;
+    totalDiscount += bagItem.original_price - bagItem.current_price;
+  });
+
+  let finalPayment = totalMRP - totalDiscount + CONVENIENCE_FEES;
+  
+
+  bagSummaryElement.innerHTML = `
+    <div class="bag-details-container">
+    <div class="price-header">PRICE DETAILS (${totalItem} Items) </div>
+    <div class="price-item">
+      <span class="price-item-tag">Total MRP</span>
+      <span class="price-item-value">₹${totalMRP}</span>
+    </div>
+    <div class="price-item">
+      <span class="price-item-tag">Discount on MRP</span>
+      <span class="price-item-value priceDetail-base-discount">-₹${totalDiscount}</span>
+    </div>
+    <div class="price-item">
+      <span class="price-item-tag">Convenience Fee</span>
+      <span class="price-item-value">₹99</span>
+    </div>
+    <hr>
+    <div class="price-footer">
+      <span class="price-item-tag">Total Amount</span>
+      <span class="price-item-value">₹${finalPayment}</span>
+    </div>
   </div>
-  <div class="price-item">
-    <span class="price-item-tag">Discount on MRP</span>
-    <span class="price-item-value priceDetail-base-discount">-Rs${totalDiscount}</span>
-  </div>
-  <div class="price-item">
-    <span class="price-item-tag">Convenience Fee</span>
-    <span class="price-item-value">Rs ${Convenience}</span>
-  </div>
-  <hr>
-  <div class="price-footer">
-    <span class="price-item-tag">Total Amount</span>
-    <span class="price-item-value">Rs ${finalPayment}</span>
-  </div>
-</div>
-<button class="btn-place-order">
+  <button class="btn-place-order">
     <div class="css-xjhrni">PLACE ORDER</div>
-  </button>`;
-
+  </button>
+  `;
 }
 
-
-function loadBagItemObjects()
-{
+function loadBagItemObjects() {
   console.log(bagItem);
-  bagItem.map(itemId => {
-    for(let i=0; i<items.length; i++)
-    {
-      if(itemId == items[i].id)
-      {
+  bagItemObjects = bagItem.map(itemId => {
+    for (let i = 0; i < items.length; i++) {
+      if (itemId == items[i].id) {
         return items[i];
       }
     }
@@ -59,9 +61,7 @@ function loadBagItemObjects()
   console.log(bagItemObjects);
 }
 
-
-function displayBagItems() 
-{
+function displayBagItems() {
   let containerElement = document.querySelector('.bag-items-container');
   let innerHTML = '';
   bagItemObjects.forEach(bagItem => {
@@ -70,54 +70,37 @@ function displayBagItems()
   containerElement.innerHTML = innerHTML;
 }
 
+function removeFromBag(itemId) {
+  bagItem = bagItem.filter(bagItemId => bagItemId != itemId);
+  localStorage.setItem('bagItems', JSON.stringify(bagItem));
+  loadBagItemObjects();
+  displayBagItem()
+  displayBagItems();
+  displayBagSummary();
+}
 
-function generateItemHTML(items)
-{
-  /*
-    return `<div class="bag-item-container">
+function generateItemHTML(item) {
+  return `<div class="bag-item-container">
     <div class="item-left-part">
-    <img class="bag-item-img" src="${items.image}">
+      <img class="bag-item-img" src="../${item.image}">
     </div>
     <div class="item-right-part">
-      <div class="company">${items.company}</div>
-      <div class="item-name">${items.item_name}</div>
+      <div class="company">${item.company}</div>
+      <div class="item-name">${item.item_name}</div>
       <div class="price-container">
-        <span class="current-price">Rs ${items.current_price}</span>
-        <span class="original-price">Rs ${items.original_price}</span>
-        <span class="discount-percentage">(${items.discount_percentage}% OFF)</span>
+        <span class="current-price">Rs ${item.current_price}</span>
+        <span class="original-price">Rs ${item.original_price}</span>
+        <span class="discount-percentage">(${item.discount_percentage}% OFF)</span>
       </div>
       <div class="return-period">
-        <span class="return-period-days">${items.return_period} days</span> return available
+        <span class="return-period-days">${item.return_period} days</span> return available
       </div>
       <div class="delivery-details">
         Delivery by
-        <span class="delivery-details-days">${items.delvery_date}</span>
-      </div>
-    </div>      
-    <div class="remove-from-cart">X</div>
-    </div>`;
-    */
-    return `<div class="bag-item-container">
-    <div class="item-left-part">
-      <img class="bag-item-img" src="${items.image}">
-    </div>
-    <div class="item-right-part">
-      <div class="company">${items.company}</div>
-      <div class="item-name">${items.item_name}</div>
-      <div class="price-container">
-        <span class="current-price">Rs ${items.current_price}</span>
-        <span class="original-price">Rs ${items.original_price}</span>
-        <span class="discount-percentage">(${items.discount_percentage}% OFF)</span>
-      </div>
-      <div class="return-period">
-        <span class="return-period-days">${items.return_period} days</span> return available
-      </div>
-      <div class="delivery-details">
-        Delivery by
-        <span class="delivery-details-days">${items.delvery_date}</span>
+        <span class="delivery-details-days">${item.delivery_date}</span>
       </div>
     </div>
 
-    <div class="remove-from-cart">x</div>
+    <div class="remove-from-cart" onclick="removeFromBag(${item.id})">X</div>
   </div>`;
-}               
+}
